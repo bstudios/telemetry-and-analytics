9b2780b68cfd44b005bc5ad1227781eac8eb4b84
James Bithell <Jbithell@users.noreply.github.com>
2023-12-31 13:19:11 +0000

    Validate form

--- app/routes/projects.adam-rms.tsx
+++ app/routes/projects.adam-rms.tsx
@@ -8,7 +8,7 @@ import { db } from "../d1client.server";
 import { Env } from "../types";
 import { AdamRMSInstallations } from "../db/schema/AdamRMSInstallations";
 import { Form, useLoaderData } from "@remix-run/react";
-import { gt } from "drizzle-orm";
+import { and, desc, eq, gt } from "drizzle-orm";
 
 export const meta: MetaFunction = () => {
   return [{ title: "AdamRMS Project | Telemetry and Analytics" }];
@@ -16,19 +16,19 @@ export const meta: MetaFunction = () => {
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
   const activeInstallations = await db((context.env as Env).DB)
-    .selectDistinct({
-      id: AdamRMSInstallations.id,
-      rootUrl: AdamRMSInstallations.rootUrl,
-    })
+    .select()
     .from(AdamRMSInstallations)
+    .groupBy(AdamRMSInstallations.rootUrl)
     .where(
-      gt(
-        AdamRMSInstallations.timestamp,
-        new Date(new Date().setDate(new Date().getDate() - 90))
+      and(
+        gt(
+          AdamRMSInstallations.timestamp,
+          new Date(new Date().setDate(new Date().getDate() - 90))
+        ),
+        eq(AdamRMSInstallations.devMode, false)
       )
     )
-    .orderBy(AdamRMSInstallations.id);
-
+    .orderBy(desc(AdamRMSInstallations.id));
   return json({ activeInstallations: activeInstallations });
 };
 
@@ -39,7 +39,7 @@ export default function Index() {
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>AdamRMS</h1>
       {activeInstallations.length > 0 ? (
         <div>
           {activeInstallations.map((activeInstallation) => (
@@ -47,6 +47,7 @@ export default function Index() {
               <a href={`/${activeInstallation.id}`}>
                 {activeInstallation.rootUrl}
               </a>
+              <pre>{JSON.stringify(activeInstallation, null, 2)}</pre>
             </div>
           ))}
         </div>
--- app/routes/projects.adam-rms.upload[.json].ts
+++ app/routes/projects.adam-rms.upload[.json].ts
@@ -9,14 +9,15 @@ import { Env } from "../types";
 import { AdamRMSInstallations } from "../db/schema/AdamRMSInstallations";
 import { withZod } from "@remix-validated-form/with-zod";
 import { z as zod } from "zod";
+import { GenericObject, validationError } from "remix-validated-form";
 
 export const loader = async ({ context }: LoaderFunctionArgs) =>
   redirect("/why-is-my-server-calling-this-url");
 
 const validator = withZod(
   zod.object({
-    rootUrl: zod.string().url(),
-    version: zod.string(),
+    rootUrl: zod.string().min(1),
+    version: zod.string().max(50).min(3),
     devMode: zod.boolean(),
     metaData: zod.object({
       instances: zod.number().or(zod.boolean()).optional(),
@@ -31,17 +32,23 @@ export const action = async ({ context, request }: ActionFunctionArgs) => {
     return json({ message: "Method not allowed" }, 405);
   }
   try {
-    var payload = await request.json();
+    const payload: unknown = await request.json();
+    var validated = await validator.validate(payload as GenericObject);
+    if (validated.error) return validationError(validated.error);
   } catch (e) {
     return json({ message: "Invalid JSON" }, 400);
   }
   const insert = await db((context.env as Env).DB)
     .insert(AdamRMSInstallations)
     .values({
-      rootUrl: payload.rootUrl,
-      version: payload.version,
-      devMode: payload.devMode,
-      metaData: payload.metaData,
+      rootUrl: validated.data.rootUrl,
+      version: validated.data.version,
+      devMode: validated.data.devMode,
+      metaData: {
+        instances: validated.data.metaData.instances || false,
+        users: validated.data.metaData.users || false,
+        assets: validated.data.metaData.assets || false,
+      },
     });
   if (insert.error) {
     return json({ message: insert.error }, 500);
